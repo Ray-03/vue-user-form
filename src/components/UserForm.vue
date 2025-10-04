de22e0c576c802afe3bcff2e8c1a6423adaee7ad
Richard <rinaldy.richard.03@gmail.com>
2025-10-04 23:39:11 +0700

    feat: add validation

--- src/components/UserForm.vue
+++ src/components/UserForm.vue
@@ -1,20 +1,28 @@
 <template>
   <n-card title="Add User">
-    <n-form :model="form" ref="formRef" label-placement="top">
+    <n-form :model="form" ref="formRef" label-placement="top" :rules="rules">
       <n-form-item label="Name" path="name">
-        <n-input placeholder="Enter name" />
+        <n-input v-model:value="form.name" placeholder="Enter name" />
       </n-form-item>
 
       <n-form-item label="Email" path="email">
-        <n-input placeholder="Enter email" />
+        <n-input v-model:value="form.email" placeholder="Enter email" />
       </n-form-item>
 
       <n-form-item label="Date of Birth" path="dob">
-        <n-date-picker type="date" placeholder="Select DOB" />
+        <n-date-picker
+          v-model:value="form.dob"
+          type="date"
+          placeholder="Select DOB"
+        />
       </n-form-item>
 
       <n-form-item label="Gender" path="gender">
-        <n-select placeholder="Select gender" :options="genderOptions" />
+        <n-select
+          v-model:value="form.gender"
+          :options="genderOptions"
+          placeholder="Select gender"
+        />
       </n-form-item>
 
       <n-space justify="end">
@@ -24,14 +32,15 @@
   </n-card>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { ref } from "vue";
+import type { FormRules } from "naive-ui";
 
 const form = ref({
   name: "",
   email: "",
-  dob: "",
-  gender: "",
+  dob: null,
+  gender: null,
 });
 
 const genderOptions = [
@@ -39,4 +48,14 @@ const genderOptions = [
   { label: "Female", value: "Female" },
   { label: "Other", value: "Other" },
 ];
+
+const rules: FormRules = {
+  name: { required: true, message: "Please input name", trigger: "blur" },
+  email: [
+    { required: true, message: "Please input email", trigger: "blur" },
+    { type: "email", message: "Invalid email", trigger: "blur" },
+  ],
+  dob: { required: true, message: "Select date of birth", trigger: "blur" },
+  gender: { required: true, message: "Select gender", trigger: "blur" },
+};
 </script>
