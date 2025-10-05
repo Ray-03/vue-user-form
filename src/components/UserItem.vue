2fd5d4a96e40e782e32d113288ffe66c2a20b0de
Richard <rinaldy.richard.03@gmail.com>
2025-10-05 09:56:22 +0700

    feat: user item

---
+++ src/components/UserItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <n-list-item>
+    <n-card>
+      <p><strong>Name:</strong> {{ user.name }}</p>
+      <p><strong>Email:</strong> {{ user.email }}</p>
+      <p><strong>DOB:</strong> {{ formatDate(user.dob) }}</p>
+      <p><strong>Gender:</strong> {{ user.gender }}</p>
+      <p><strong>Created At:</strong> {{ formatDate(user.createdAt) }}</p>
+      <p><strong>Updated At:</strong> {{ formatDate(user.updatedAt) }}</p>
+
+      <n-space justify="end">
+        <n-button
+          size="small"
+          type="error"
+          tertiary
+          @click="$emit('delete', user.id)"
+        >
+          Delete
+        </n-button>
+      </n-space>
+    </n-card>
+  </n-list-item>
+</template>
+
+<script setup lang="ts">
+const user: any = {};
+const formatDate = (date: Date) => new Date(date).toLocaleDateString();
+</script>
