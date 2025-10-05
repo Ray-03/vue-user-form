dfc7dc1fb937c76bd37a762d8435de8678dfc724
Richard <rinaldy.richard.03@gmail.com>
2025-10-05 09:54:47 +0700

    feat: add user list template

---
+++ src/components/UserList.vue
@@ -0,0 +1,13 @@
+<template>
+  <n-card title="User List">
+    <n-empty v-if="!users.length" description="No users yet" />
+
+    <n-list v-else>
+      <user-item v-for="user in users" :key="user.id" :user="user" />
+    </n-list>
+  </n-card>
+</template>
+
+<script setup lang="ts">
+const users: any[] = [];
+</script>
