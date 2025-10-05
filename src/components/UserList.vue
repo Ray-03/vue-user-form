2d4b3ea584d7b0c60aad1ffaf3f971e466d3c775
Richard <rinaldy.richard.03@gmail.com>
2025-10-05 13:37:21 +0700

    feat: implement store

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -3,11 +3,23 @@
     <n-empty v-if="!users.length" description="No users yet" />
 
     <n-list v-else>
-      <user-item v-for="user in users" :key="user.id" :user="user" />
+      <user-item
+        v-for="user in users"
+        :key="user.id"
+        :user="user"
+        @delete="handleDelete"
+      />
     </n-list>
   </n-card>
 </template>
 
 <script setup lang="ts">
-const users: any[] = [];
+import { computed } from "vue";
+import { useUserStore } from "../stores/UserStore";
+import UserItem from "./UserItem.vue";
+
+const userStore = useUserStore();
+const users = computed(() => userStore.users);
+
+const handleDelete = (id: string) => userStore.deleteUser(id);
 </script>
