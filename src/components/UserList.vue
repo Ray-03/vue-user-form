c50bc5164adf2ee5ac69e011f6de13cb5dd0f0c2
Richard <rinaldy.richard.03@gmail.com>
2025-10-05 21:14:40 +0700

    feat: add grid and list view

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -1,28 +1,67 @@
 <template>
   <n-card title="User List">
+    <template #header-extra>
+      <n-button-group>
+        <n-button
+          :type="viewMode === VIEW_MODE.LIST ? 'primary' : 'default'"
+          @click="viewMode = VIEW_MODE.LIST"
+        >
+          <template #icon>
+            <n-icon>
+              <ListAltOutlined />
+            </n-icon>
+          </template>
+        </n-button>
+        <n-button
+          :type="viewMode === VIEW_MODE.GRID ? 'primary' : 'default'"
+          @click="viewMode = VIEW_MODE.GRID"
+        >
+          <template #icon>
+            <n-icon>
+              <GridViewOutlined />
+            </n-icon>
+          </template>
+        </n-button>
+      </n-button-group>
+    </template>
+
     <n-spin :show="loading">
       <n-empty v-if="!users.length && !loading" description="No users yet" />
 
-      <n-list v-else>
+      <n-list v-else-if="viewMode === VIEW_MODE.LIST">
         <user-item
           v-for="user in users"
           :key="user.id"
           :user="user"
+          :view-mode="viewMode"
           @delete="handleDelete"
         />
       </n-list>
+
+      <div v-else class="grid-container">
+        <user-item
+          v-for="user in users"
+          :key="user.id"
+          :user="user"
+          :view-mode="viewMode"
+          @delete="handleDelete"
+        />
+      </div>
     </n-spin>
   </n-card>
 </template>
 
 <script setup lang="ts">
-import { computed, onMounted } from "vue";
+import { computed, onMounted, ref } from "vue";
 import { useUserStore } from "../stores/UserStore";
 import UserItem from "./UserItem.vue";
+import { ListAltOutlined, GridViewOutlined } from "@vicons/material";
+import { VIEW_MODE } from "../types/ViewMode";
 
 const userStore = useUserStore();
 const users = computed(() => userStore.users);
 const loading = computed(() => userStore.loading);
+const viewMode = ref<VIEW_MODE>(VIEW_MODE.LIST);
 
 onMounted(() => {
   userStore.fetchUsers();
@@ -30,3 +69,11 @@ onMounted(() => {
 
 const handleDelete = (id: string) => userStore.deleteUser(id);
 </script>
+
+<style scoped>
+.grid-container {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 16px;
+}
+</style>
